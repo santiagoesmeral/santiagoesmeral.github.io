07b775ca471f00415195f0eed22328561db171d7
santiagoesmeral <santiagoesmeral@hotmail.com>
2022-08-07 19:45:04 -0300

    added a first draft of the about me text

--- src/pages/about_me/AboutMe.tsx
+++ src/pages/about_me/AboutMe.tsx
@@ -2,28 +2,75 @@ import "./AboutMe.scss";
 export default function AboutMe() {
   return (
     <section className="about-me">
-      <h1>Lorem Ipsum</h1>
+      <h1>Hello! I am Santiago Esmeral</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        I am a semisenior react developer from Argentina, looking for new
+        oportunities.
+      </p>
+
+      <h2>Who i am as a programmer</h2>
+      <p>
+        I can be described with two sentences: I make big emphasis in making my
+        code understandable to other programmers, and I dont like to rush a
+        botch implementation
       </p>
       <br />
-      <h2>Doloret</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        If there is one thing i've learned over the many projects i've worked
+        on, is that making an easy to understand piece of code can be crucial in
+        the longevity of a project. The reality of the industry is that projects
+        often have multiple programmers working on them throughout their
+        lifetime, with differing skills and experiences. We often assume that an
+        implementation we do something is obvious, whereas it may not be obvious
+        for an intern, or for another fellow developer who learned to use
+        different libraries or methodologies.
       </p>
       <br />
-      <h2>Asimet</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        This is why, when i write code, i try to make it as accesible as
+        possible. I like to write documentation, and simplify code. I try to
+        adhere to baseline HTML elements and simple yet carefully crafted CSS
+        selectors whenever possible, as those are standards set by the web
+        which, once you've learned them, you'll be able to use them anywhere.
+      </p>
+      <br />
+      <p>
+        When i write code, i also like to make sure that i am implementing the
+        best possible solutions. I often spend hours reading documentation and
+        searching the web for what other programmers have done to solve a given
+        situation. I belive the quality of the implementation far outweighs
+        delivery speed. Many bugs occur when we rush things without trying to
+        understand the problem in depth.
+      </p>
+
+      <h2>My Knowledge</h2>
+      <p>
+        I have a lot of experience with legacy and new versions of React, as
+        i've worked with many of the libraries which can be considered industry
+        standards (like react-router, ajax and ag-grid). I've worked with
+        Typescript, SCSS, Redux and TailwindCSS (even if i dont recomend the
+        later 2) I also know about web accesibility and a bit of SEO.
+      </p>
+
+      <h2>My Experience, in a nutshell</h2>
+      <p>
+        I began working in JPMorgan Chase in an internship, where i learned the
+        basics of javascript, reactjs and web development in general. My
+        internship lasted for a year and half. In JPMorgan, i worked in both
+        maintenance and updating old proyects, as well as creating and
+        maintaining an entire app from scratch. Unfortunately, due to the global
+        pandemic, many job positions previously open were closed down, including
+        that of my entire team. And so, i found a job in Eulas, a software
+        factory under Minutentag. In Eulas, i worked in two projects. In the
+        first one, i worked on creating and improving already existing
+        components which were going to be reused across the application. In the
+        second one, i worked as a contractor for an ecommerce website,
+        implementing a shipping interface for purchases Then, i began working on
+        smaller projects as a Freelancer. Some of the projects i've worked on
+        include implementing login systems and visualization tools in an
+        advertisement analytics apps and implementing react v18 code in an
+        angularJS (yes, the old version) for a slow transition to updating the
+        app
       </p>
     </section>
   );
