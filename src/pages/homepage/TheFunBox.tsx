fd48ecf4833318362d6a57cb2542aa98ad474f0b
santiagoesmeral <santiagoesmeral@hotmail.com>
2022-08-27 19:25:51 -0300

    fix bug with interval

--- src/pages/homepage/TheFunBox.tsx
+++ src/pages/homepage/TheFunBox.tsx
@@ -1,69 +1,74 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useInterval from "../../usefulFunctions/useInterval";
 import "./TheFunBox.scss";
 /*
     This is a very special and weird component: it represents the box in the homepage that will display all sort of different things. From cheatsheets to animations, to minigames.
+
+    The idea: have the page loop over a couple of random minigames, except when the user is selecting the links or the searchbar. 
+    If they are selecting those, then instead show the user a cheatsheet with some useful info
 */
+
+const randomEvents = ["unclickableButton"];
 
 export default function TheFunBox() {
   const [currentStatus, setCurrentStatus] = useState("");
-  useEffect(() => {
+
+  function handleCurrentHoverOrFocusElement() {
     /*
     !I am not sure if this code is worth optimizing too much
     At the time of writing, i tested execution time on the interval function.
     The longest time reported, which is a rare occasion, shows 20ms of execution time. 
-
+    
     ...that is 0.02 seconds.
-
+    
     If this was code that i would put in a business project i would be worried, but to be fair, its not that bad. 
     I dont think its worth neither the effort nor the inevitable code mess it will become due to trying to optimize it.
-
-    TODO: 
-      -add a check to make sure the page is currently the active tab, otherwise do nothing
-      -add checks for redundant calls. No need to re-run the setState code if the state has not changed at all.
-      -test on other browsers and intentionally reduce the speed of the browser to make sure its still ok
     
-    EDIT: currently working on the hover effect. I have 2 options, neither of which i really like
-      1- add a useless :hover property to the element i want to listen to, and every loop check in the document if that class + hover pseudoclass exists (dont like it, relies too much on css)
-      2- attach an onmousemove function to the document, then every loop get the X and Y coordinates of the event given to the function, then use the function document.elementFromPoint with the X and Y coordinates to get the element we're hovering over
+    TODO: test on other browsers and intentionally reduce the speed of the browser to make sure its still ok
     */
 
-    let currentHoverElements: Element[] | null;
-    function setCurrentHoverElementOnMouseMove(event: any) {
-      currentHoverElements = document.elementsFromPoint(
-        event.clientX,
-        event.clientY
-      );
+    //dont wanna be calling this if the page isnt currently active
+    if (document.hidden) return;
+
+    const currentActiveElem = document.activeElement;
+
+    //separated this if statement from the others so that we only declare isListOfLinksHovered if needed
+    if (currentActiveElem?.id === "skip-navigation-target") {
+      //skip-navigation-target, in the homepage, represent the searchbar. This element will only be rendered in the home page so this effectively means the searchbar
+      if (currentStatus !== "searchbarCheatsheet") {
+        setCurrentStatus("searchbarCheatsheet");
+        return;
+      } else return;
     }
-    document.onmousemove = setCurrentHoverElementOnMouseMove;
 
-    function handleCurrentHoverOrFocusElement() {
-      const currentActiveElem = document.activeElement;
+    /*
+      I tried my best to make the hover logic as clean as possible. 
+      But any solution leads down a path of madness.
+      Either you make an absolute mess with mouse event logics in the component,
+      or you make a mess by setting a var that captures the onmousemove from the document.
 
-      if (currentActiveElem?.id === "skip-navigation-target") {
-        //skip-navigation-target, in the homepage, represent the searchbar. This element will only be rendered in the home page so this effectively means the searchbar
-        setCurrentStatus("showSearchbarCheatsheet");
-      } else if (
-        currentActiveElem?.id.includes("homepage-link-card-number-") ||
-        currentHoverElements?.some(
-          (elem: Element) => elem.id === "homepage-list-of-links"
-        )
-      ) {
-        setCurrentStatus("showNumpadCheatsheet");
-      } else {
-        setCurrentStatus("");
+      ...or you make this. At the time of calling the function, you get the list of all items with the hovered status, then convert it to an array and check if any of them matches the id of the list of links.
+      
+      If anyone can provide a better alternative, then let me know
+    */
+    const isListOfLinksHovered = Array.from(
+      document.querySelectorAll(":hover")
+    ).some((elem: Element) => elem.id === "homepage-list-of-links");
+
+    if (
+      currentActiveElem?.id.includes("homepage-link-card-number-") ||
+      isListOfLinksHovered
+    ) {
+      if (currentStatus !== "numpadCheatsheet") {
+        setCurrentStatus("numpadCheatsheet");
       }
-    }
-
-    const interval = window.setInterval(handleCurrentHoverOrFocusElement, 300);
-
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+    } else setCurrentStatus("unclickableButton");
+  }
+  useInterval(handleCurrentHoverOrFocusElement, 300);
 
   const elementToRender = () => {
     switch (currentStatus) {
-      case "showSearchbarCheatsheet":
+      case "searchbarCheatsheet":
         return (
           <div className="the-fun-box-cheatsheet">
             <p className="the-fun-box-cheatsheet-decription">
@@ -86,7 +91,7 @@ export default function TheFunBox() {
             </div>
           </div>
         );
-      case "showNumpadCheatsheet":
+      case "numpadCheatsheet":
         return (
           <div className="the-fun-box-cheatsheet">
             <p className="the-fun-box-cheatsheet-decription">
@@ -98,8 +103,16 @@ export default function TheFunBox() {
             </p>
           </div>
         );
+      case "unclickableButton":
+        return (
+          <div className="the-fun-box-unclickable-button-container">
+            <button>ᕕ(⌐■_■)ᕗ ♪♬</button>
+          </div>
+        );
+      case "nothing":
+        return "";
       default:
-        return <div className="TEMP-IDK-WHAT-TO-CALL-THIS"></div>;
+        return "";
     }
   };
   return (
