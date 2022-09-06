75162fb3d9601838442f4772fc07fbb209d66ef2
santiagoesmeral <santiagoesmeral@hotmail.com>
2022-09-06 14:33:25 -0300

    move hire_me button outside of popup, change styling of buttons and logic of focus for the popup

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, Fragment, useEffect } from "react";
 import "./Header.scss";
 
 export default function Header() {
   //i could do css shenanigans instead of state, but it will complicate things for what i have in mind for mobile
   //todo: close menu when page is no longer active
+  //todo: close menu when clicking outside of it (not entirely sure about this one)
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    /*  
+      In theory, useEffect runs after every render. So, rather than creating a whole new component which can be annoying with typescript, i can instead some logic for the opening and closing of the menu here
+    */
+    //when the menu gets opened, set the focus on the first button
+    if (isMenuOpen) {
+      document.getElementById("header-nav-link-0")?.focus();
+    } else {
+      //when the menu is closed, focus on the open menu button
+      document.getElementById("header-open-nav-button")?.focus();
+    }
+  }, [isMenuOpen]);
 
   const menu = () => {
     //TODO: continue this
@@ -20,6 +34,7 @@ export default function Header() {
       return (
         <nav className="header-nav">
           <button
+            id="header-nav-link-0"
             title="Close Navigation"
             className="header-button header-close-navigation-button"
             onClick={() => setIsMenuOpen(false)}
@@ -27,7 +42,7 @@ export default function Header() {
             Close Menu
           </button>
           <a
-            id="header-link-1"
+            id="header-nav-link-1"
             href="/hire_me"
             className="header-button"
             title="Hire Me"
@@ -35,7 +50,7 @@ export default function Header() {
             Hire Me
           </a>
           <a
-            id="header-link-2"
+            id="header-nav-link-2"
             href="https://github.com/santiagoesmeral"
             className="header-button"
             title="Santiago Esmeral's Github profile"
@@ -43,7 +58,7 @@ export default function Header() {
             My Github Profile
           </a>
           <a
-            id="header-link-3"
+            id="header-nav-link-3"
             href="https://www.linkedin.com/in/santiago-alfredo-esmeral-albarracin-37250516b/"
             className="header-button"
             title="Santiago Esmeral's LinkedIn profile"
@@ -51,6 +66,7 @@ export default function Header() {
             My LinkedIn Profile
           </a>
           <button
+            id="header-nav-link-4"
             className="header-button"
             title="Santiago Esmeral's Codepen (coming soon)"
             disabled
@@ -58,6 +74,7 @@ export default function Header() {
             My Codepen
           </button>
           <button
+            id="header-nav-link-5"
             className="header-button"
             title="Download my CV (coming soon)"
             disabled
@@ -68,14 +85,25 @@ export default function Header() {
       );
     } else
       return (
-        <button
-          title="Open Navigation"
-          className="header-button header-open-navigation-button"
-          onClick={() => setIsMenuOpen(true)}
-          aria-haspopup
-        >
-          ☰
-        </button>
+        <Fragment>
+          <a
+            id="header-hire-me-button"
+            href="/hire_me"
+            className="header-button hire-me-quick-access-button"
+            title="Hire Me"
+          >
+            Hire Me
+          </a>
+          <button
+            id="header-open-nav-button"
+            title="Open Navigation"
+            className="header-button header-open-navigation-button"
+            onClick={() => setIsMenuOpen(true)}
+            aria-haspopup
+          >
+            ☰
+          </button>
+        </Fragment>
       );
   };
 
@@ -89,10 +117,11 @@ export default function Header() {
   };
   return (
     <header className="header">
-      <a id="header-link-0" className="header-title" href="/">
+      <a id="header-homepage-link" className="header-title" href="/">
         Santiago Esmeral
       </a>
       <button
+        id="header-skip-nav-button"
         className="header-button hidden-button"
         onClick={() => onSkipNavigation()}
       >
